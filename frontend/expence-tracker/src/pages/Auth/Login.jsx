3f7b2452615dc388f133e458adaa070d159f7d94
maymichaeli <maushe55@gmail.com>
2025-04-08 14:04:30 +0300

    AuthLayouts component and login page

--- frontend/expence-tracker/src/pages/Auth/Login.jsx
+++ frontend/expence-tracker/src/pages/Auth/Login.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import AuthLayouts from "../../components/layouts/AuthLayouts"
+import CARD_2 from "../../assets/images/card2.png"
 
 const Login = () =>{
   return(
-    <div>Login</div>
+    <AuthLayouts>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to log in</p>
+      </div>
+    </AuthLayouts>
   )
 }
 
