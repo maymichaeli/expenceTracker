8b8b033cfaa11842282c633eed68c1ad98db8b8f
maymichaeli <maushe55@gmail.com>
2025-04-08 14:41:10 +0300

    log in and password errors

--- frontend/expence-tracker/src/pages/Auth/Login.jsx
+++ frontend/expence-tracker/src/pages/Auth/Login.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import AuthLayouts from "../../components/layouts/AuthLayouts"
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () =>{
   const [email,setEmail] = useState("");
@@ -12,7 +13,24 @@ const Login = () =>{
   const navigate= useNavigate();
 
   //Handle Login Form Submit
-  const handleLogin = async (e) =>{}
+  const handleLogin = async (e) =>{
+    e.preventDefault();
+
+    if( !validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login API call
+  }
+
   return(
     <AuthLayouts>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -26,6 +44,25 @@ const Login = () =>{
            label="Email Address"
            placeholder="john@example.com"
            type="text" />
+           <Input 
+           value={password} 
+           onChange={({ target })=> setPassword(target.value)}
+           label="Password"
+           placeholder="Min 8 Characters"
+           type="password" />
+
+           {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+           <button type="submit" className="btn-primary">
+            LOGIN
+           </button>
+
+           <p className="text-[13px] text-slate-800 mt-3">
+              Don't have an account?{" "}
+              <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+              </Link>
+           </p>
         </form>
       </div>
     </AuthLayouts>
